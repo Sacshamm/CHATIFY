5f900ba2f485b09698e23264de3228ba32c9fa08
Saksham Sharma <sakshamsharma648@gmail.com>
2023-07-09 17:05:38 +0530

    deployment link

--- server/index.js
+++ server/index.js
@@ -32,6 +32,7 @@ const server = app.listen(process.env.PORT, () =>
 const io = socket(server, {
   cors: {
     // origin: "http://localhost:3000",
+    // origin: "http://localhost:3000",
     origin: "http://chatify-pied.vercel.app",
     credentials: true,
   },
