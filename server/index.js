36c78622bf411890729abaa1236477f83403cfa4
Saksham Sharma <sakshamsharma648@gmail.com>
2023-07-09 18:21:33 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -31,8 +31,8 @@ const server = app.listen(process.env.PORT, () =>
 
 const io = socket(server, {
   cors: {
-    // origin: "http://localhost:3000",
-    origin: "http://chatify-pied.vercel.app",
+    origin: "http://localhost:3000",
+    // origin: "http://chatify-pied.vercel.app",
     credentials: true,
   },
 });
